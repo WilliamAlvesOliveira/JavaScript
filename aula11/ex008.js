c0987f71bac92cf9de286823dfb430174fe854dc
WilliamAlvesOliveira <27will.ramone@gmail.com>
2024-11-27 12:06:20 -0300

    aula 11

---
+++ aula11/ex008.js
@@ -0,0 +1,8 @@
+var vel = 12
+console.log(`A velocidade do seu carro é ${vel} km/h`)
+
+if(vel > 60){
+    console.log('Você ultrapassou a velocidade permitida, MULTADO1')
+}
+
+console.log('Dirija sempre usando o sinto de segurança!')
---
+++ aula11/ex009.js
@@ -0,0 +1,7 @@
+var pais = 'BraSil'
+
+if(pais.toUpperCase() == 'BRASIL'){
+    console.log('Você é brasileiro!')
+}else{
+    console.log('Você  é estrangeiro!')
+}
