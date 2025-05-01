6b8f890d1f1175e888bbb55c96a7539c023f0740
WilliamAlvesOliveira <27will.ramone@gmail.com>
2025-05-01 13:39:09 -0300

    comentários
    comentários para futuras consultas

--- Serliv/exercicios/exercicio-calcularIMC/callback.js
+++ Serliv/exercicios/exercicio-calcularIMC/callback.js
@@ -1,3 +1,12 @@
+/*exercicio
+criar uma função calcularImc para calcular o IMC de uma pessoa recebendo peso, altura e uma função classificarIMC, caso a função seja chamada sem passar algum dos parametros deve retornar 0, caso receba apenas o peso e altura retorna o IMC, se for passada a função deve retornar o resultado.
+entradas:
+() // 0,
+(60, 1.64) // 22.3081 ,
+(60, 1.64, classificarIMC) // peso normal
+*/
+
+
 function calcularIMC(peso, altura, action){
     if (typeof peso !== 'number' || typeof altura !== 'number'){ return 0 }
     console.log(peso, altura)
@@ -8,16 +17,19 @@ function calcularIMC(peso, altura, action){
     return IMC
 }
 
-const classificarIMC = (IMC) =>{return
-    IMC <= 16.9 ? "Muito abaixo do peso" :
+const classificarIMC = (IMC) =>{
+    return IMC <= 16.9 ? "Muito abaixo do peso" :
     IMC <= 18.4 ? "Abaixo do peso" :
     IMC <= 24.9 ? "Peso normal" :
     IMC <= 29.9 ? "Sobrepeso" :
     IMC <= 34.9 ? "Obesidade Grau I" :
     IMC <= 40   ? "Obesidade Grau II" : "Obesidade Grau III"};
   
-
-console.log(calcularIMC())
-console.log(calcularIMC(60, 1.64))
+//chamada sem parametro retorno 0
+console.log(calcularIMC()) 
+//chamada sem o atributo action para retorna somento o imc
+console.log(calcularIMC(60, 1.64)) 
+//chada com o atributo action retorna a classificação
 console.log(calcularIMC(60, 1.64, classificarIMC))
+//chamada da função classificarimc diretamente
 console.log(classificarIMC(35))
