d6e85b29b4b85921aff43fda17b8067cd8280399
WilliamAlvesOliveira <27will.ramone@gmail.com>
2025-07-22 09:52:52 -0300

    Curso Serliv - Generics
    aula sobre uso de 2 ou mais tipos genéricos e uso de restrição

--- Serliv/typescript/Generics/src/interface-type.ts
+++ Serliv/typescript/Generics/src/interface-type.ts
@@ -22,3 +22,32 @@ type ActionType<T> = {
 const action3 :ActionType<boolean> ={
     step1: true
 }
+
+//================================
+// Utilizando 2 ou mais tipos em
+//       um único Generic
+//================================
+
+interface Action2 <T = string, U = number>{
+    action: T
+    timestamp: U
+}
+
+const act: Action2 = {
+    action: 'delete',
+    timestamp: 123
+}
+
+//restren
+//podemos restringir os tipos utilizando :
+// <T extends string | number, U = number> 
+//também podemos restringir e manter o default
+// <T extends string | number = string, U = number>
+// e ainda podemos setar um type com os tipos para usalos depois
+//type NumberOrString = number | string
+//<T extends NumberOrString = string, U = number>
+
+interface Action3 <T extends string | number = string, U = number>{
+    action: T
+    timestamp: U
+}
